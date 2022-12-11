0dcafaab32a3c7f9a89a793aa2fbccd755956590
sabinakanimetova5@gmail.com <sabinakanimetova5@gmail.com>
2022-12-11 19:43:01 +0600

    hw2,я не смогла уладить ошибку с crit. В интернете сказали новую версию скачать и переустановить setup.py. Но у меня возникли проблемы

--- hero.py
+++ hero.py
@@ -17,7 +17,7 @@ class SuperHero():
         print(f'health: {self.health_points * 2}')
 
     def __str__(self):
-        return f'name: {self.nickname}\n' \
+        return f'nickname: {self.nickname}\n' \
                f'superpower: {self.superpower}\n' \
                f'health: {self.health_points}'
 
@@ -27,6 +27,77 @@ class SuperHero():
 
 Hero = SuperHero(name="Sabina", nickname="02", superpower="stupidity", health_points=50, catchphrase="I wanna sleep")
 Hero.print_name_hero()
+print(Hero)
 Hero.print_health()
 Hero.__len__()
-print(Hero)
+
+
+class Batman(SuperHero):
+    ears = "ears"
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False ):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly=fly
+        self.damage=damage
+    def print_health(self):
+        self.fly = True
+        print(f'health: {self.health_points ** 2}')
+
+    def print_fly(self):
+        print(f'fly in the True_phrase')
+
+bat = Batman(name="Emma", nickname="kani", superpower="my brain", health_points=60, catchphrase="I wanna eat", damage=5)
+print(bat)
+bat.print_health()
+bat.print_fly()
+
+
+
+class Goodman(SuperHero):
+    nails = "nails"
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly=fly
+        self.damage=damage
+
+    def print_health(self):
+        self.fly = True
+        print(f'health: {self.health_points ** 2}')
+    def print_fly(self):
+        print(f'fly in the True_phrase')
+
+
+
+good = Goodman(name="Emily", nickname="bugaga", superpower="teleport", health_points=70, catchphrase="Меня зовут Кира Йошикаге", damage=11)
+print(good)
+good.print_fly()
+good.print_health()
+
+
+class Villian(Goodman):
+    people = "monster"
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+
+    def gen_x(self):
+        pass
+
+    def crit(self, damage):
+        print(f'damage: {self.damage ** 6}')
+
+
+vivi = Villian(name="L", nickname="Lll", superpower="cloaning", health_points=100, catchphrase="LOL", damage=21)
+print(vivi)
+vivi.gen_x()
+vivi.crit(good.damage)
+
+
+
+
+
+
+
+
+
+
+
